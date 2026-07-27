a25c84422bd7dadb8414b5a0e1914a8e57d7d255
Gourav-Desetty <139993532+Gourav-Desetty@users.noreply.github.com>
2026-07-27 12:43:04 +0530

    implement log file parser

--- backend/parser.py
+++ backend/parser.py
@@ -1,19 +1,34 @@
 import re
+from typing import List, Dict
 
-def parse_logs(log_lines):
-    result = []
-    for log in log_lines:
-        match = re.search(
-            r"\[(.*?)\]\s+(\d+)\s+(\S+)\s+-\s+(\w+)\s+-\s+(.*)",
-            log,
-        )
-        if match:
-            result.append({
-                "timestamp": match.group(1),
-                "line": match.group(2),
-                "logger": match.group(3),
-                "level": match.group(4),
-                "message": match.group(5),
-            })
 
-    return result
+class Parser:
+    LOG_PATTERN = re.compile(
+        r"\[(.*?)\]\s+(\d+)\s+(\S+)\s+-\s+(\w+)\s+-\s+(.*)"
+    )
+    def parse(self, log_lines: List[str]) -> List[Dict]:\
+
+        parsed_logs = []
+
+        for log in log_lines:
+            parsed_log = self._parse_log(log)
+
+            if parsed_log:
+                parsed_logs.append(parsed_log)
+
+        return parsed_logs
+
+    def _parse_log(self, log: str) -> Dict | None:
+
+        match = self.LOG_PATTERN.search(log)
+
+        if not match:
+            return None
+
+        return {
+            "timestamp": match.group(1),
+            "line": match.group(2),
+            "logger": match.group(3),
+            "level": match.group(4),
+            "message": match.group(5),
+        }
