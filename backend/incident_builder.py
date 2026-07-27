eb9a84a8fd7386a499476371a7730a403d7b083e
Gourav-Desetty <139993532+Gourav-Desetty@users.noreply.github.com>
2026-07-27 12:42:43 +0530

    implement incident report builder

--- backend/incident_builder.py
+++ backend/incident_builder.py
@@ -1,36 +1,39 @@
 from datetime import datetime
 from typing import List, Dict
 
+class IncidentBuilder:
+    def build(
+        self,
+        raw_logs: List[str],
+        parsed_logs: List[Dict],
+        detection_result: Dict,
+    ) -> Dict:
 
-def build_incident(
-    raw_logs: List[str],
-    parsed_logs: List[Dict],
-    detection_result: Dict
-) -> Dict:
+        return {
+            "incident": detection_result["incident"],
+            "severity": detection_result["severity"],
+            "generated_at": datetime.now().isoformat(),
 
-    incident = {
-        "incident": detection_result["incident"],
-        "severity": detection_result["severity"],
-        "generated_at": datetime.now().isoformat(),
+            "counts": detection_result["counts"],
+            "messages": detection_result["messages"],
 
-        "counts": detection_result["counts"],
-        "messages": detection_result["messages"],
+            "total_logs": len(raw_logs),
 
-        "total_logs": len(raw_logs),
+            "first_log_time": self._get_first_log_time(parsed_logs),
+            "last_log_time": self._get_last_log_time(parsed_logs),
 
-        "first_log_time": (
-            parsed_logs[0]["timestamp"]
-            if parsed_logs else None
-        ),
+            "parsed_logs": parsed_logs,
+            "raw_logs": raw_logs,
+        }
 
-        "last_log_time": (
-            parsed_logs[-1]["timestamp"]
-            if parsed_logs else None
-        ),
+    def _get_first_log_time(self, parsed_logs: List[Dict]):
+        if not parsed_logs:
+            return None
 
-        "parsed_logs": parsed_logs,
+        return parsed_logs[0]["timestamp"]
 
-        "raw_logs": raw_logs
-    }
+    def _get_last_log_time(self, parsed_logs: List[Dict]):
+        if not parsed_logs:
+            return None
 
-    return incident
+        return parsed_logs[-1]["timestamp"]
